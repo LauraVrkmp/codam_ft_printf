9d52925a63f7deb4b5bd7de430836b4485a8ed03
Laura Veerkamp <laveerka@f1r5s12.codam.nl>
2025-11-19 17:07:44 +0100

    impl of int to hex

--- ft_pointer_hex.c
+++ ft_pointer_hex.c
@@ -6,7 +6,7 @@
 /*   By: laveerka <laveerka@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/11/19 14:50:20 by laveerka      #+#    #+#                 */
-/*   Updated: 2025/11/19 15:32:07 by laveerka      ########   odam.nl         */
+/*   Updated: 2025/11/19 16:00:09 by laveerka      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,22 @@
 static int	convert_write_hex(unsigned long addr)
 {
 	char	*hex_values;
+	char	str[17];
+	int		length;
+	int		i;
 
+	length = 0;
+	i = 16;
+	str[i--] = '\0';
 	hex_values = "0123456789abcdef";
+	while (addr > 0)
+	{
+		str[i--] = hex_values[addr % 16];
+		addr /= 16;
+	}
 	
-	return (0);
+	printf("%s\n", str);
+	return (length);
 }
 
 int	print_pointer(va_list arg)
